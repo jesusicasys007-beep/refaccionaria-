3133480939e2a9e1e0d9b1a01fdf61dc251fb655
jesusicasys007-beep <jesusicasys007@gmail.com>
2026-03-08 11:05:26 -0600

    Update Brand.php
    Co-Authored-By: joseran0025-del <264831658+joseran0025-del@users.noreply.github.com>

--- ProyectoJesus_Jose/app/Models/Brand.php
+++ ProyectoJesus_Jose/app/Models/Brand.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+   use HasFactory;
+    protected $fillable = [
+     'name',
+     "slug",
+     'country',
+     'description',
+     'website',
+    ]; 
 }
